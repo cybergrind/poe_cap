30f5a12a490ddef279a11bed0f556fbf97b59b69
Kirill Pinchuk <cybergrind@gmail.com>
2024-12-28 18:47:12 +0300

    better cross-platform naming

--- other/socket_hook/socket_hook.c
+++ other/socket_hook/socket_hook.c
@@ -8,7 +8,6 @@
 // hook all socket calls
 
 int socket(int domain, int type, int protocol) {
-    printf("hhhhhhh");
     static int (*real_socket)(int, int, int) = NULL;
     if (!real_socket) {
         real_socket = dlsym(RTLD_NEXT, "socket");
@@ -21,35 +20,14 @@ int socket(int domain, int type, int protocol) {
 }
 
 #ifdef __APPLE__
-int connect2(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
-    FILE *fd = fopen("/tmp/socket.log", "a");
-
-    if (addr->sa_family == AF_INET) {
-        struct sockaddr_in *addr_in = (struct sockaddr_in *) addr;
-        char *ip = inet_ntoa(addr_in->sin_addr);
-        int port = ntohs(addr_in->sin_port);
-        fprintf(fd, "connect() called %s %i\n", ip, port);
-    } else if (addr->sa_family == AF_INET6) {
-        struct sockaddr_in6 *addr_in6 = (struct sockaddr_in6 *) addr;
-        char ip[INET6_ADDRSTRLEN];
-        inet_ntop(AF_INET6, &addr_in6->sin6_addr, ip, INET6_ADDRSTRLEN);
-        int port = ntohs(addr_in6->sin6_port);
-        fprintf(fd, "connect() called %s %i\n", ip, port);
-    } else if (addr-> sa_family == AF_UNIX) {
-        struct sockaddr_un *addr_un = (struct sockaddr_un *) addr;
-        fprintf(fd, "connect() called for UNIX socket %s\n", addr_un->sun_path);
-    } else {
-        fprintf(fd, "connect() called unknown address family %i\n", addr->sa_family);
-    }
-    return connect(sockfd, addr, addrlen);
-}
+int connect_hook(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 #else
-int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+inline int connect_hook(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
     static int (*real_connect)(int, const struct sockaddr *, socklen_t) = NULL;
     if (!real_connect) {
         real_connect = dlsym(RTLD_NEXT, "connect");
     }
-
+#endif
     FILE *fd = fopen("/tmp/socket.log", "a");
 
     if (addr->sa_family == AF_INET) {
@@ -69,9 +47,12 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
     } else {
         fprintf(fd, "connect() called unknown address family %i\n", addr->sa_family);
     }
+#ifdef __APPLE__
+    return connect(sockfd, addr, addrlen);
+#else
     return real_connect(sockfd, addr, addrlen);
-}
 #endif
+}
 
 #ifdef __APPLE__
 #define DYLD_INTERPOSE(_replacement, _replacee) \
@@ -81,5 +62,9 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
     } _interpose_##_replacee \
     __attribute__ ((section ("__DATA,__interpose"))) = { (const void *)(unsigned long)&_replacement, (const void *)(unsigned long)&_replacee };
 
-DYLD_INTERPOSE(connect2, connect);
+DYLD_INTERPOSE(connect_hook, connect);
+#else
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    return connect_hook(sockfd, addr, addrlen);
+}
 #endif
